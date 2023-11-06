55313088115c1b7b40c75fdeffc5edb0d38f5707
Roman Annenkov <roman.annenkov@gmail.com>
2023-11-06 21:49:54 +0300

    added code written on seminarThree

--- seminarThree/src/test/java/tdd/UserTest.java
+++ seminarThree/src/test/java/tdd/UserTest.java
@@ -1,10 +1,29 @@
 package tdd;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class UserTest {
+    private User user;
+
+    @BeforeEach
+    void setup() {
+        user = new User("Roma","testpass", true);
+    }
+
+    @Test
+    void authenticationTestPass() {
+        assertTrue(user.authenticate("Roma","testpass"));
+    }
+
+    @Test
+    void authenticationTestFailed() {
+        assertFalse(user.authenticate("admin","123123"));
+    }
+
+
 }
